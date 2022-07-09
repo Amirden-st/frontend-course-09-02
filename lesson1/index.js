efba640e2166b488e896bedd2f9459f0fd1fe42b
Amirden-st <82529791+Amirden-st@users.noreply.github.com>
2022-07-09 12:29:30 +0600

    initial commit

--- lesson1/index.js
+++ lesson1/index.js
@@ -3,58 +3,49 @@
 // переменная
 // object
 var Adi = {
-    // property - св-во
-    name: 'Adilet',
-    age: 18,
-    language: 'ru',
-    'prop prop': 'value'
-}
+  // property - св-во
+  name: "Adilet",
+  age: 18,
+  language: "ru",
+  "prop prop": "value"
+};
 // number
-var number = 1
+var number = 1;
+
 // string
-var string = 'string'
+var string = "string";
 // boolean
-var True = true
-var False = false
+var True = true;
+var False = false;
 
 // undefined
-var variable
+var variable;
 
-// null 
-var myNull = null
+// null
+var myNull = null;
 // console.log(variable)
 
-
-
 // 1
-console.log(Adi.language)
+console.log(Adi.language);
 // 2
-console.log(Adi['prop prop'])
-
-
+console.log(Adi["prop prop"]);
 
 // operators: -, +, *,/, <, >, ==/=== typeof....
 
-var myName = 'Adilet'
+var myName = "Adilet";
 // console.log(myName.length)
 // console.log(typeof myName)
 //          выражение
 // console.log(typeof '1')
-// if 
+// if
 if (false) {
-    console.log('Всё верно')
-} 
-
-else if (myName.length < 4) {
-    console.log('Нужно больше 4 символов')
+  console.log("Всё верно");
+} else if (myName.length < 4) {
+  console.log("Нужно больше 4 символов");
+} else if (myName === "Adilet") {
+  console.log("Hello, Adilet");
+} else {
+  console.log("Вводи строку");
 }
-else if (myName === 'Adilet') {
-    console.log('Hello, Adilet')
-}
-
-else {
-    console.log('Вводи строку')
-}
-
 
 // console.log(number)
